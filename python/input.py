8f3433f488ed0367fb8a0a9ba7680c763365932b
Amy Batina <aebatina@yahoo.com>
2016-10-18 16:13:20 -0400

    4

--- python/input.py
+++ python/input.py
@@ -2,5 +2,5 @@
 #print('Hi ' + name)
 #name = raw_input('What is your name?')
 age = raw_input('How old are you?')
-dog_years = age * 7
-print('You are ' + dog_years + 'old in dog years.')
+dog_years = int(age) * 7
+print('You are ' + str(dog_years) + 'years old in dog years.')
