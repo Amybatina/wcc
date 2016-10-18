663dd5f13e316edc0027e474df0c1daa376acf66
Amy Batina <aebatina@yahoo.com>
2016-10-18 16:33:38 -0400

    again

--- python/input.py
+++ python/input.py
@@ -5,4 +5,4 @@ meal_cost = float(raw_input('How much was your meal?'))
 tip = int(meal_cost) * .20
 total_price = int(meal_cost) + int(tip)
 print('You should tip $' + str(tip)
-print('Your total cost would be $' + str(total_price) + (tip))
+print('Your total cost would be $' + str(total_price) + str(tip)
