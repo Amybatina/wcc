19f7682046d2dfa59323368462317e72abd3d5a1
Amy Batina <aebatina@yahoo.com>
2016-10-18 16:38:53 -0400

    ugh

--- python/input.py
+++ python/input.py
@@ -3,6 +3,6 @@
 #name = raw_input('What is your name?')
 meal_cost = float(raw_input('How much was your meal?'))
 tip = int(meal_cost) * .20
-total_price = int(meal_cost) + int(tip)
+total_price = float(meal_cost) + float(tip)
 print('You should tip $' + str(tip)
 print('Your total cost would be $' + str(total_price) + str(tip)
