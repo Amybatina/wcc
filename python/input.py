121ce809a40dbc2738fe57620cff0dbff52563d3
Amy Batina <aebatina@yahoo.com>
2016-10-18 15:39:01 -0400

    try

--- python/input.py
+++ python/input.py
@@ -1,2 +1,5 @@
-name = raw_input('What is your name? ')
-print('Hi ' + name)
+#name = raw_input('What is your name? ')
+#print('Hi ' + name)
+name = raw_input('What is your name?')
+age = raw_input('How old are you?')
+print(name + ' is ' + age + ' years old.')
