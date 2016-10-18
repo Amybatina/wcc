7207d2c5694e78172871892c74ccf47235ef7005
Amy Batina <aebatina@yahoo.com>
2016-10-18 17:46:08 -0400

    next

--- python/madlib.py
+++ python/madlib.py
@@ -1,1 +1,12 @@
 print('Welcome to the Mad Lib Generator!')
+
+adjective1 = raw_input('Type in an adjective: ')
+adjective2 = raw_input('Another adjective: ')
+adjective3 = raw_input('Add another adjective: ')
+adjective4 = raw_input('Last adjective: ')
+plural_animal = raw_input('Plural animal (e.g., puppies): ')
+verb = raw_input('Now a verb: ')
+plural_noun = raw_input('Now a plural noun: ')
+name = raw_input('The first name of a woman musician: ')
+beverage = raw_input('A beverage: ')
+plural_body_part = raw_input('And finally, a plural body part: ')
