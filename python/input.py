22df6dfcf9779e06fa7e797089ee21758cae92c6
Amy Batina <aebatina@yahoo.com>
2016-10-18 16:25:49 -0400

    test

--- python/input.py
+++ python/input.py
@@ -1,6 +1,7 @@
 #name = raw_input('What is your name? ')
 #print('Hi ' + name)
 #name = raw_input('What is your name?')
-age = raw_input('How old are you?')
-dog_years = int(age) * 7
-print('You are ' + str(dog_years) + 'years old in dog years.')
+meal_cost = raw_input('How much was your meal?')
+tip = float(meal_cost) * .20
+print('You should tip ' + float(tip))
+print('Your total cost would be ' + float(meal_cost) + float(tip))
