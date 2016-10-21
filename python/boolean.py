2a206393e430059885a3175a6651e56682f9e76c
Amy Batina <aebatina@yahoo.com>
2016-10-20 22:26:17 -0400

    ex3

--- python/boolean.py
+++ python/boolean.py
@@ -19,3 +19,24 @@ print(10 < age) # Expected outcome: True
 print(age > 10 + 20) # Expected outcome: False
 
 print(age + 20 > 10) # Expected outcome: True
+age = 1
+print(age > 12 and age < 19) # Expected outcome:
+
+age = 14
+print(age > 12 and age < 19) # Expected outcome: True
+
+age = 19
+print(age > 12 and age < 19) # Expected outcome: False
+
+age = 18
+print(age > 12 and age < 19 and age != 5) # Expected outcome: True
+
+age = 5
+print(age > 12 and age < 19 and age != 5) # Expected outcome: False
+
+age = -1
+print(age > 12 and age < 19) # Expected outcome: False
+
+age = 10
+print(age > 25 and age < 15) # Expected outcome:    False
+# Could the above expression ever be True?
