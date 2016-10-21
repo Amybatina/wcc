75d0f7bd56cbcb922d12a6d25daf4874a2914057
Amy Batina <aebatina@yahoo.com>
2016-10-20 22:18:47 -0400

    ex1

---
+++ python/boolean.py
@@ -0,0 +1,11 @@
+print(1 == 2) # Expected output: False
+
+print(1 > 2) # Expected output: False
+
+print(2 > 1) # Expected output: True
+
+print(1 >= 1) # Expected output: True
+
+print(2 == 2) # Expected output:True
+
+print(2 != 2) # Expected output: False
