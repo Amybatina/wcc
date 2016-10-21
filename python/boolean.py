d01c4fe28477007500f5e1a9ba23746288bc1e7e
Amy Batina <aebatina@yahoo.com>
2016-10-20 22:21:22 -0400

    ex2

--- python/boolean.py
+++ python/boolean.py
@@ -9,3 +9,13 @@ print(1 >= 1) # Expected output: True
 print(2 == 2) # Expected output:True
 
 print(2 != 2) # Expected output: False
+
+age = 30
+
+print(age > 10) # Expected outcome: True
+
+print(10 < age) # Expected outcome: True
+
+print(age > 10 + 20) # Expected outcome: False
+
+print(age + 20 > 10) # Expected outcome: True
