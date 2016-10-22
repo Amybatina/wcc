e8cbec3d03d83f73b35d04483b28a36f4d358a75
Amy Batina <aebatina@yahoo.com>
2016-10-21 21:26:16 -0400

    attempt

--- python/blackjack.py
+++ python/blackjack.py
@@ -16,4 +16,18 @@ computer_card1 = cards.pop()
 print('Player card: ' + str(player_card1))
 print('Computer card:  ' + str(computer_card1))
 
-print(cards) # To see the list after two cards have been popped off.
+#print(cards) # To see the list after two cards have been popped off.
+decision = raw_input('\nIf you want to stay type `s`, if you want to hit type `h`: ')
+
+# Round 2
+player_card1 = cards.pop()
+computer_card_1 = cards.pop()
+
+print('Your card: ' + str(player_card1))
+print('computer card:  ' + str(computer_card_1))
+
+decision = raw_input('\nIf you want to stay type `s`, if you want to hit type `h`: ')
+if decision == 's'
+print('Your card: '0')
+if decision == 'h'
+player_card2 = cards.pop()
