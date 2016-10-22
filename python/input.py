5a1813c0ef674839e8f54c80b95a0d8cc7c6853e
Amy Batina <aebatina@yahoo.com>
2016-10-21 20:31:29 -0400

    test

--- python/input.py
+++ python/input.py
@@ -5,5 +5,5 @@ meal_cost = float(raw_input('How much was your meal?'))
 tip = meal_cost * .20
 total_price = meal_cost + tip
 
-print('You should tip $' + str(tip))
+print('You did not enter a valid option, defaulted to 20%')
 print('Your total cost would be $' + str(total_price))
