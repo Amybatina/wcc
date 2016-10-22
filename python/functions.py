e29279e39fe6e2960191253a6fd18b771c4fdf7e
Amy Batina <aebatina@yahoo.com>
2016-10-21 22:10:09 -0400

    here

---
+++ python/functions.py
@@ -0,0 +1,7 @@
+def multiply(a, b):
+    result = a * b
+    return result
+
+# Test the function:
+solution = multiply(4, 5) # Invoke multiply giving it the arguments 4 and 5
+print(solution) # Expected: 20
