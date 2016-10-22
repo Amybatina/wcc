b6bec4210137f1fa6c9d8a2ce571b22705181237
Amy Batina <aebatina@yahoo.com>
2016-10-21 21:44:39 -0400

    try again

--- python/blackjack.py
+++ python/blackjack.py
@@ -1,33 +1,101 @@
-import random
+#import random
 
 #                                        J   Q   K   A
-cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+#cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 
-print(cards) # To see the list before being shuffled
+#print(cards) # To see the list before being shuffled
 
-random.shuffle(cards)
+#random.shuffle(cards)
 
 #print(cards) # To see the list after being shuffled
+
+# Round 1
+#player_card1 = cards.pop()
+#computer_card1 = cards.pop()
+
+#print('Player card: ' + str(player_card1))
+#print('Computer card:  ' + str(computer_card1))
+
+#print(cards) # To see the list after two cards have been popped off.
+#decision = raw_input('\nIf you want to stay type `s`, if you want to hit type `h`: ')
+
+# Round 2
+#player_card1 = cards.pop()
+#computer_card_1 = cards.pop()
+
+#print('Your card: ' + str(player_card1))
+#print('computer card:  ' + str(computer_card_1))
+
+#decision = raw_input('\nIf you want to stay type `s`, if you want to hit type `h`: ')
+#if decision == 's'
+#    print('Your card: '0')
+#if decision == 'h'
+#    player_card2 = cards.pop()
+
+import random
+
+# Set up
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+random.shuffle(cards)
+
+
 
 # Round 1
 player_card1 = cards.pop()
 computer_card1 = cards.pop()
 
-print('Player card: ' + str(player_card1))
+print('Your card: ' + str(player_card1))
 print('Computer card:  ' + str(computer_card1))
 
-#print(cards) # To see the list after two cards have been popped off.
 decision = raw_input('\nIf you want to stay type `s`, if you want to hit type `h`: ')
+
+
 
 # Round 2
-player_card1 = cards.pop()
-computer_card_1 = cards.pop()
+if decision == 'h':
+    player_card2 = cards.pop()
+else:
+    player_card2 = 0
 
-print('Your card: ' + str(player_card1))
-print('computer card:  ' + str(computer_card_1))
+computer_card2 = cards.pop()
+
+print('Your cards: ' + str(player_card1) + ", " + str(player_card2))
+print('Computer cards: ' + str(computer_card1) + ", " + str(computer_card2))
 
 decision = raw_input('\nIf you want to stay type `s`, if you want to hit type `h`: ')
-if decision == 's'
-    print('Your card: '0')
-if decision == 'h'
-    player_card2 = cards.pop()
+
+
+
+# Round 3
+if decision == 'h':
+    player_card3 = cards.pop()
+else:
+    player_card3 = 0
+
+computer_total = computer_card1 + computer_card2
+if computer_total < 16:
+    computer_card3 = cards.pop()
+else:
+    computer_card3 = 0
+
+print('Your cards: ' + str(player_card1) + ", " + str(player_card2) + ", " + str(player_card3))
+print('Computer cards: ' + str(computer_card1) +  ", " + str(computer_card2) + ", " + str(computer_card3))
+
+# Results
+player_total = player_card1 + player_card2 + player_card3
+computer_total = computer_card1 + computer_card2 + computer_card3
+
+print('\nGame over...')
+
+if player_total > 21 and computer_total > 21:
+    print('Tie! (Both Player and Computer busted.)')
+elif player_total == computer_total:
+    print('Tie!')
+elif computer_total > 21:
+    print('You win! (Computer busted.)')
+elif player_total > 21:
+    print('Computer wins. (You busted.)')
+elif player_total > computer_total:
+    print('You win! (You were closest to 21.)')
+else:
+    print('Computer wins. (It was closest to 21.)')
